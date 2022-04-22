0ada4e9ac0f97ebc65d8260975013011dd767329
Sr-316 <sr-3.16@outlook.com>
2022-04-22 05:42:58 +0000

    transformed to tradeplat4m-postgresql

--- sql/001_init.sql
+++ sql/001_init.sql
@@ -1,15 +1,59 @@
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS AppUser (
-    id                      varchar (256) PRIMARY KEY,
-    username                varchar (256) NOT NULL,
-    email                   varchar (256) NOT NULL,
-    password                varchar (256) NOT NULL,
-    role                    varchar (256) NULL,
-    CONSTRAINT              UK_APPUSER_USERNAME UNIQUE (username),
-    CONSTRAINT              UK_APPUSER_EMAIL UNIQUE (email)
+CREATE TABLE ticker (
+	id BIGSERIAL PRIMARY KEY,
+	symbol TEXT NOT NULL
 );
-
-INSERT INTO AppUser
-(id, username, email, password, role) VALUES (uuid_generate_v4(), 'admin', 'admin@domain.com', 'admin', 'ROLE_ADMIN');
+CREATE TABLE ticker_price (
+  ticker_id INTEGER NOT NULL,
+  dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  open DOUBLE PRECISION NOT NULL,
+  high DOUBLE PRECISION NOT NULL,
+  low DOUBLE PRECISION NOT NULL,
+  close DOUBLE PRECISION NOT NULL,
+  volume DOUBLE PRECISION NOT NULL,
+  PRIMARY KEY (ticker_id, dt),
+  CONSTRAINT fk_symbol FOREIGN KEY (ticker_id) REFERENCES ticker(id)
+);
+CREATE INDEX ON ticker_price (ticker_id, dt DESC);
+CREATE TABLE tick_data (
+  ticker_id INTEGER NOT NULL,
+  dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  bid NUMERIC NOT NULL,
+  ask NUMERIC NOT NULL,
+  bid_vol NUMERIC,
+  ask_vol NUMERIC,
+  PRIMARY KEY (ticker_id, dt),
+  CONSTRAINT fk_symbol FOREIGN KEY (ticker_id) REFERENCES ticker(id)
+);
+CREATE INDEX ON tick_data (ticker_id, dt DESC);
+SELECT
+  create_hypertable('ticker_price', 'dt');
+SELECT
+  create_hypertable('tick_data', 'dt');
+CREATE MATERIALIZED VIEW daily_bars WITH (timescaledb.continuous) AS
+SELECT
+  ticker_id,
+  time_bucket(INTERVAL '1 day', dt) AS day,
+  first(open, dt) as open,
+  max(high) as high,
+  min(low) as low,
+  last(close, dt) as close,
+  sum(volume) as volume
+FROM
+  ticker_price
+GROUP BY
+  ticker_id,
+  day;
+CREATE MATERIALIZED VIEW "hourly_bars" WITH (timescaledb.continuous) AS
+SELECT
+  ticker_id,
+  time_bucket(INTERVAL '1 hour', dt) AS hour,
+  first(open, dt) as open,
+  max(high) as high,
+  min(low) as low,
+  last(close, dt) as close,
+  sum(volume) as volume
+FROM
+  ticker_price
+GROUP BY
+  ticker_id,
+  hour;
